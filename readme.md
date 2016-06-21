da0bc1dcef78a7682cf127d11bae592bd4160596
#云淡然 <cloudcome@163.com>
2016-06-21 17:50:20 +0800

    tips 组件

---
+++ readme.md
@@ -0,0 +1,15 @@
+# blear.components.tips
+
+[![npm module][npm-img]][npm-url]
+[![build status][travis-img]][travis-url]
+[![coverage][coveralls-img]][coveralls-url]
+
+[travis-img]: https://img.shields.io/travis/blearjs/blear.components.tips/master.svg?style=flat-square
+[travis-url]: https://travis-ci.org/blearjs/blear.components.tips
+
+[npm-img]: https://img.shields.io/npm/v/blear.components.tips.svg?style=flat-square
+[npm-url]: https://www.npmjs.com/package/blear.components.tips
+
+[coveralls-img]: https://img.shields.io/coveralls/blearjs/blear.components.tips/master.svg?style=flat-square
+[coveralls-url]: https://coveralls.io/github/blearjs/blear.components.tips?branch=master
+
---
+++ src/index.js
@@ -0,0 +1,51 @@
+/**
+ * tips 组件
+ * @author ydr.me
+ * @create 2016-05-30 21:13
+ */
+
+
+'use strict';
+
+
+var Tips = require('blear.ui.tips');
+var typeis = require('blear.utils.typeis');
+
+
+var buildExports = function (type) {
+    return function (message) {
+        var options = {};
+
+        if (typeis.Object(message)) {
+            options = message;
+        } else {
+            message = message || '';
+            message = message && message.message ? message.message : message;
+            options.message = message;
+        }
+
+        options.type = type;
+        var tips = new Tips(options);
+
+        tips.on('sure', function () {
+            tips.destroy(function () {
+                tips = null;
+            });
+        });
+
+        return tips.open();
+    };
+};
+
+
+
+exports.default = buildExports('default');
+exports.success = buildExports('success');
+exports.danger = buildExports('danger');
+exports.info = buildExports('info');
+exports.warn = buildExports('warn');
+
+
+
+
+
